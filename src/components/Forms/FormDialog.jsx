3642b3f126cb56f4268d0181d7c918d79661c821
RYOTA <kobaryo04ysh@icloud.com>
2020-10-16 16:05:01 +0900

    move dataset to firebase

--- src/components/Forms/FormDialog.jsx
+++ src/components/Forms/FormDialog.jsx
@@ -37,7 +37,28 @@ export default class FormDialog extends React.Component {
       const email = this.state.email;
       const description = this.state.description;
 
-      
+      const payload = {
+        text: 'お問い合わせがありました\n' + 
+              'お名前:' + name + '\n' +
+              'Email:' + email + '\n' +
+              'お問い合わせ内容:\n' + description 
+      }
+
+      const url = 'https://hooks.slack.com/services/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'
+
+      fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(payload)
+      }).then(() => {
+        alert('送信が完了しました。追ってご連絡いたします!')
+        this.setState({
+          name: "",
+          email: "",
+          description: ""
+        })
+        return this.props.handleClose()
+      })
+
     }
 
     render() {
---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA1IBdgcGMkJ1e67nGjKwJdIq6UbHfiWN8",
+  authDomain: "chatbot-ed3a7.firebaseapp.com",
+  databaseURL: "https://chatbot-ed3a7.firebaseio.com",
+  projectId: "chatbot-ed3a7",
+  storageBucket: "chatbot-ed3a7.appspot.com",
+  messagingSenderId: "576156663105",
+  appId: "1:576156663105:web:7ac775a19cdd73984375c0",
+  measurementId: "G-VX33ZYE4QH"
+};
+
+export default firebaseConfig;
