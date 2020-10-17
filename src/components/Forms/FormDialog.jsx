fef53cfed1887a87f392be6efdcc91a31b683618
RYOTA <kobaryo04ysh@icloud.com>
2020-10-17 22:42:56 +0900

    remove api key

--- src/components/Forms/FormDialog.jsx
+++ src/components/Forms/FormDialog.jsx
@@ -59,7 +59,7 @@ const FormDialog = (props) => {
                 + '【問い合わせ内容】\n' + description
         };
        
-        const url = 'https://hooks.slack.com/services/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'
+        
 
         fetch(url, {
           method: 'POST',
